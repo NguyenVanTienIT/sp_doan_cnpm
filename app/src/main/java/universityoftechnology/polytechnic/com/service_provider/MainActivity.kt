bf6da97cc9b7bb04a3a42d956314f384fd473fe5
DESKTOP-9FRLS76\Asus <tiennguyenvan1408@gmail.com>
2019-02-24 15:50:46 +0700

    login activity

--- app/src/main/java/universityoftechnology/polytechnic/com/service_provider/MainActivity.kt
+++ app/src/main/java/universityoftechnology/polytechnic/com/service_provider/MainActivity.kt
@@ -1,12 +1,53 @@
 package universityoftechnology.polytechnic.com.service_provider
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+
+    var optionalUser : Spinner? = null
+    var editTextMaNhaHang : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        optionalUser = findViewById(R.id.option_user)
+        editTextMaNhaHang = findViewById(R.id.ma_nha_hang)
+
+        optionalUser!!.setSelection(0, true)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.optionl_user,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            optionalUser!!.adapter = adapter
+
+        }
+
+        optionalUser!!.onItemSelectedListener = this
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if (position == 0){
+            editTextMaNhaHang!!.visibility = View.VISIBLE
+        }
+        else{
+            editTextMaNhaHang!!.visibility = View.GONE
+        }
     }
 }
